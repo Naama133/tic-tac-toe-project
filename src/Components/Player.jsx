531690d4c80e4e3bde54edbd6e9a5b188698d993
Naama133 <naamab@wix.com>
2024-03-21 15:31:59 +0200

    Two-Way-Binding

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol})
+export default function Player({ initialName, symbol})
 {
-    const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditClick()
-    {
-        setIsEditing(true);
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
+
+    function handleEditClick(){
+        setIsEditing((IsEditing) => !IsEditing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
-    if(isEditing) {playerName = <input type="text" required></input>}
+    function handleChange(event)
+    {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    if(isEditing) {editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}></input>}
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
 
-            <button onClick={handleEditClick}> Edit </button>
+            <button onClick={handleEditClick}> { isEditing ? 'Save' : 'Edit' } </button>
         </li>
     );
 }
